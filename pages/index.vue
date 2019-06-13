f906cda6e52a207e24d5b6c849a2195cf4836c3d
Karmov <a.karmov@pugofka.com>
2019-06-13 18:38:19 +0300

    feat: frontend FORM

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,65 @@
 <template>
   <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6></v-flex>
+    <v-flex xs12 sm8>
+      <v-card min-width="400">
+        <v-card-title>
+          <h1>Nuxt чат</h1>
+        </v-card-title>
+        <v-card-text>
+          <v-form ref="form" v-model="valid" lazy-validation>
+            <v-text-field v-model="name" :counter="16" :rules="nameRules" label="Ваше имя" required></v-text-field>
+
+            <v-text-field v-model="room" :rules="roomRules" label="Введите комнату" required></v-text-field>
+
+            <v-btn :disabled="!valid" color="primary" @click="submit">Войти</v-btn>
+          </v-form>
+        </v-card-text>
+      </v-card>
+    </v-flex>
   </v-layout>
 </template>
 
 <script>
+import { mapMutations } from "vuex";
 export default {
-  components: {}
+  layout: "empty",
+  head: {
+    title: "Добро пожаловать на Nuxt чат"
+  },
+  sockets: {
+    connect: function() {
+      console.log("socket connected");
+    },
+    customEmit: function(data) {
+      console.log(
+        'this method was fired by the socket server. eg: io.emit("customEmit", data)'
+      );
+    }
+  },
+  data: () => ({
+    valid: true,
+    name: "",
+    nameRules: [
+      v => !!v || "Введите имя",
+      v => (v && v.length <= 16) || "Имя не должно превышать 16 символов"
+    ],
+    room: "",
+    roomRules: [v => !!v || "Введите комнату"]
+  }),
+
+  methods: {
+    ...mapMutations(["setUser"]),
+    submit() {
+      if (this.$refs.form.validate()) {
+        const user = {
+          name: this.name,
+          room: this.room
+        };
+
+        this.setUser(user);
+        this.$router.push("/chat");
+      }
+    }
+  }
 };
 </script>
